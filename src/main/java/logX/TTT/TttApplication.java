fc0bec9ba09c035f895aa46ebcaf25068cfe81c5
Karmesin924 <jinhong0924@naver.com>
2024-07-02 14:59:21 +0900

    Init: 프로젝트 초기화 & DB설정

---
+++ src/main/java/logX/TTT/TttApplication.java
@@ -0,0 +1,13 @@
+package logX.TTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/logX/TTT/TttApplicationTests.java
@@ -0,0 +1,13 @@
+package logX.TTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
