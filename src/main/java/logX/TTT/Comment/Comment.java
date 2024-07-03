3d1b7eea6d2df54c53e2bf5383f83f0f690316b3
Karmesin924 <jinhong0924@naver.com>
2024-07-03 15:16:31 +0900

    feat: Post, Comment 엔티티 작성

---
+++ src/main/java/logX/TTT/Comment/Comment.java
@@ -0,0 +1,38 @@
+package logX.TTT.Comment;
+
+import jakarta.persistence.*;
+import logX.TTT.Member.Member;
+import logX.TTT.Post.Post;
+import lombok.AllArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    Post post;
+}
---
+++ src/main/java/logX/TTT/Post/Post.java
@@ -0,0 +1,42 @@
+package logX.TTT.Post;
+
+import jakarta.persistence.*;
+import logX.TTT.Comment.Comment;
+import logX.TTT.Member.Member;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+
+}
