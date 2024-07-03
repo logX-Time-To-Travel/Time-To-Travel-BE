e3f3f7d33c9a378d42bc361660c0c19ea9c5724f
Karmesin924 <jinhong0924@naver.com>
2024-07-03 15:17:12 +0900

    feat: Location 엔티티 작성

---
+++ src/main/java/logX/TTT/Location/Location.java
@@ -0,0 +1,29 @@
+package logX.TTT.Location;
+
+import jakarta.persistence.*;
+import logX.TTT.Post.Post;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    @OneToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
