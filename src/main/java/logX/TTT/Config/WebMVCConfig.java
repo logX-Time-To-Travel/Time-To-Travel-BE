57632afdf3ed2780047a117a51b3952d3e35a927
Karmesin924 <jinhong0924@naver.com>
2024-07-09 10:24:55 +0900

    feat: 파일 업로드 환경설정

---
+++ src/main/java/logX/TTT/Config/WebMVCConfig.java
@@ -0,0 +1,11 @@
+package logX.TTT.Config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+    }
+}
