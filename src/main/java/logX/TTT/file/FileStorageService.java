1e5280adfe5e2ab16a4cf05abdcea30ad6c52d05
Karmesin924 <jinhong0924@naver.com>
2024-07-09 10:53:55 +0900

    feat: 파일 업로드 구현 (Service, Controller)

---
+++ src/main/java/logX/TTT/file/FileStorageService.java
@@ -0,0 +1,27 @@
+package logX.TTT.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileStorageService {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    public String storeFile(MultipartFile file) throws IOException {
+        Path filePath = Paths.get(uploadDir, file.getOriginalFilename());
+        Files.createDirectories(filePath.getParent());
+        Files.copy(file.getInputStream(), filePath);
+        return filePath.toString();
+    }
+
+    public String getFileUrl(String filename) {
+        return "/image"+ filename;
+    }
+}
---
+++ src/main/java/logX/TTT/file/FileUploadController.java
@@ -0,0 +1,31 @@
+package logX.TTT.file;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/image")
+public class FileUploadController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file) {
+        try {
+            String filePath = fileStorageService.storeFile(file);
+            String fileUrl = fileStorageService.getFileUrl(filePath);
+            return ResponseEntity.ok(fileUrl);
+        }
+        catch(IOException e) {
+            return ResponseEntity.status(500).body("파일 업로드 실패");
+        }
+    }
+}
