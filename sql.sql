b0c0e9bee22ac152874a86a273190f21f9f08588
Karmesin924 <jinhong0924@naver.com>
2024-07-12 15:22:57 +0900

    style: SQL 코드 포맷 변경

--- sql.sql
+++ sql.sql
@@ -1,71 +1,87 @@
-CREATE TABLE member (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at DATE NOT NULL,
+CREATE TABLE member
+(
+    id                INT AUTO_INCREMENT PRIMARY KEY,
+    username          VARCHAR(255) NOT NULL,
+    email             VARCHAR(255) NOT NULL,
+    password          VARCHAR(255) NOT NULL,
+    created_at        DATE         NOT NULL,
     profile_image_url VARCHAR(255),
-    total_likes INT DEFAULT 0,
-    total_views INT DEFAULT 0
+    total_likes       INT DEFAULT 0,
+    total_views       INT DEFAULT 0
 );
 
-CREATE TABLE post (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    member_id INT,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
+
+CREATE TABLE post
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    member_id  INT,
+    title      TEXT      NOT NULL,
+    content    TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id)
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE content (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    `index` INT NOT NULL,
-    data TEXT NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES post(id)
+
+CREATE TABLE content
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT          NOT NULL,
+    type    VARCHAR(255) NOT NULL,
+    `index` INT          NOT NULL,
+    data    TEXT         NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
-CREATE TABLE location (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT NOT NULL,
-    latitude DECIMAL(10, 7) NOT NULL,
+
+CREATE TABLE location
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    post_id   INT            NOT NULL,
+    latitude  DECIMAL(10, 7) NOT NULL,
     longitude DECIMAL(10, 7) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES post(id)
+    name      VARCHAR(255)   NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
-CREATE TABLE comment (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT NOT NULL,
-    member_id INT NOT NULL,
-    content TEXT NOT NULL,
+
+CREATE TABLE comment
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    post_id    INT       NOT NULL,
+    member_id  INT       NOT NULL,
+    content    TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES post(id),
-    FOREIGN KEY (member_id) REFERENCES member(id)
+    FOREIGN KEY (post_id) REFERENCES post (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE likes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT NOT NULL,
+
+CREATE TABLE likes
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    post_id   INT NOT NULL,
     member_id INT NOT NULL,
     UNIQUE (post_id, member_id),
-    FOREIGN KEY (post_id) REFERENCES post(id),
-    FOREIGN KEY (member_id) REFERENCES member(id)
+    FOREIGN KEY (post_id) REFERENCES post (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE views (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    post_id INT NOT NULL,
+
+CREATE TABLE views
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    post_id   INT NOT NULL,
     member_id INT,
     FOREIGN KEY (post_id) REFERENCES post (id),
     FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE interests(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    member_id INT NOT NULL,
-    interest VARCHAR(255) NOT NULL,
+
+CREATE TABLE interests
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT          NOT NULL,
+    interest  VARCHAR(255) NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member (id)
 );
+
