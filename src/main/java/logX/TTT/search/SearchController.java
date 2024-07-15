1443259bca5dd4a7fd67066c8ae7b64250571836
Karmesin924 <jinhong0924@naver.com>
2024-07-15 14:27:03 +0900

    feat: Search Controller 작성 - 사용자의 검색 결과 저장 api

---
+++ src/main/java/logX/TTT/search/SearchController.java
@@ -0,0 +1,26 @@
+package logX.TTT.search;
+
+import logX.TTT.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+    @Autowired
+    private SearchService searchService;
+    @Autowired
+    private MemberService memberService;
+
+    @PostMapping("/{username}")
+    public ResponseEntity<Void> saveSearchQuery(@PathVariable String username, @RequestBody String query) {
+        Long memberId = memberService.getMemberIdByUsername(username);
+        searchService.saveSearchQuery(memberId, query);
+        return ResponseEntity.ok().build();
+    }
+}
