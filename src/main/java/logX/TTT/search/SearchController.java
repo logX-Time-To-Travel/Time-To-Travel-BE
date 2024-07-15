a1b647b5420ce07f0fb32e3d6df10031211e5121
Karmesin924 <jinhong0924@naver.com>
2024-07-15 14:38:40 +0900

    fix: Search Controller - 검색 기록 전송 시 json 형식 맞춰서 받기

--- src/main/java/logX/TTT/search/SearchController.java
+++ src/main/java/logX/TTT/search/SearchController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/search")
 public class SearchController {
@@ -18,7 +20,8 @@ public class SearchController {
     private MemberService memberService;
 
     @PostMapping("/{username}")
-    public ResponseEntity<Void> saveSearchQuery(@PathVariable String username, @RequestBody String query) {
+    public ResponseEntity<Void> saveSearchQuery(@PathVariable String username, @RequestBody Map<String, String> requestBody) {
+        String query = requestBody.get("keyword");
         Long memberId = memberService.getMemberIdByUsername(username);
         searchService.saveSearchQuery(memberId, query);
         return ResponseEntity.ok().build();
