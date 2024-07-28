481641fa321737b4b24bbb8db892e68bb73f4043
Karmesin924 <jinhong0924@naver.com>
2024-07-28 13:25:29 +0900

    feat: CORS 설정 - localhost:5173만 허용

--- src/main/java/logX/TTT/config/WebMvcConfig.java
+++ src/main/java/logX/TTT/config/WebMvcConfig.java
@@ -1,11 +1,24 @@
 package logX.TTT.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true);
     }
 }
