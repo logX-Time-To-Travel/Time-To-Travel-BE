8d36f94ad641fb1d472f85281a46c22c5cec1f7c
Minsu Yoo <minsoo0515@naver.com>
2024-08-06 13:47:43 +0900

    fix : 글 작성 날짜 추가

--- src/main/java/logX/TTT/post/model/PostSummaryDTO.java
+++ src/main/java/logX/TTT/post/model/PostSummaryDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +15,5 @@ public class PostSummaryDTO {
     private int likeCount;
     private int viewCount;
     private String imageUrl;
+    private LocalDateTime createdAt;
 }
