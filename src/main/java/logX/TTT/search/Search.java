2f5ff5489fa1d69228c49d2b0ab9f8357bb82d9e
Karmesin924 <jinhong0924@naver.com>
2024-08-21 14:52:44 +0900

    fix: Search Entity - searchedHistoryId 칼럼에 id 이름 추가

--- src/main/java/logX/TTT/search/Search.java
+++ src/main/java/logX/TTT/search/Search.java
@@ -17,6 +17,7 @@ import java.time.LocalDateTime;
 public class Search {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long searchHistoryId;
 
     @ManyToOne
