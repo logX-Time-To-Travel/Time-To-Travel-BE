043af0f93958bd6627507143259e01c0e764f411
Karmesin924 <jinhong0924@naver.com>
2024-08-31 16:41:46 +0900

    feat: 좋아요 누른 게시물 리스트 반환 Controller 작성

--- src/main/java/logX/TTT/likes/LikesController.java
+++ src/main/java/logX/TTT/likes/LikesController.java
@@ -1,16 +1,23 @@
 package logX.TTT.likes;
 
+import logX.TTT.member.Member;
+import logX.TTT.member.MemberService;
+import logX.TTT.post.model.PostSummaryDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/likes")
+@RequiredArgsConstructor
 public class LikesController {
 
-    @Autowired
-    private LikesService likesService;
+    private final LikesService likesService;
+    private final MemberService memberService;
 
     @PostMapping("/{postId}/{memberId}")
     public ResponseEntity<Void> likePost(@PathVariable Long postId, @PathVariable Long memberId) {
@@ -29,6 +36,17 @@ public class LikesController {
             return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<List<PostSummaryDTO>> getLikePosts(@PathVariable Long memberId) {
+        try {
+            Member member = memberService.getMemberById(memberId);
+            List<PostSummaryDTO> posts = likesService.getLikedPostsByMember(member);
+            return ResponseEntity.ok(posts);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).build();
         }
     }
 }
